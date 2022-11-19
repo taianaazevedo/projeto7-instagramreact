c239fa62e468fd04510f6749093a3a95cc6be7b3
taianaazevedo <taianazev@gmail.com>
2022-11-19 17:32:25 -0300

    tags data-test colocadas

--- src/components/Post.js
+++ src/components/Post.js
@@ -15,10 +15,10 @@ export default function Post() {
 
 function EachPost(props){
     return(
-        <div class="post">
+        <div class="post" data-test="post">
                 <div class="topo">
                     <div class="usuario">
-                        <img src={props.imgUser} />
+                        <img src={props.imgUser} data-test="post-image"/>
                         {props.userName}
                     </div>
                     <div class="acoes">
@@ -33,18 +33,18 @@ function EachPost(props){
                 <div class="fundo">
                     <div class="acoes">
                         <div>
-                            <ion-icon name="heart-outline"></ion-icon>
+                            <ion-icon name="heart-outline" data-test="like-post"></ion-icon>
                             <ion-icon name="chatbubble-outline"></ion-icon>
                             <ion-icon name="paper-plane-outline"></ion-icon>
                         </div>
                         <div>
-                            <ion-icon name="bookmark-outline"></ion-icon>
+                            <ion-icon name="bookmark-outline" data-test="save-post"></ion-icon>
                         </div>
                     </div>
 
                     <div class="curtidas">
                         <img src={props.likedBy} />
-                        <div class="texto">
+                        <div class="texto" data-test="likes-number">
                             {props.text}
                         </div>
                     </div>
--- src/components/Usuario.js
+++ src/components/Usuario.js
@@ -1,16 +1,16 @@
 
 export default function Usuario() {
-    const changeName = () => {prompt("Qual seu nome de usuário?")}
+    
 
 
     return (        
-            <div class="usuario">
-                <img src="assets/img/catanacomics.svg" />
+            <div class="usuario" data-test="user">            
+                <img src="assets/img/catanacomics.svg" data-test="profile-image"/>
                 <div class="texto">
                     <strong>catanacomics</strong>
-                    <span>
+                    <span data-test="name">
                         Catana
-                        <ion-icon name="pencil" onClick={changeName}></ion-icon>
+                        <ion-icon name="pencil" data-test="edit-name"></ion-icon>
                     </span>
                 </div>
             </div>
