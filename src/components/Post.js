4dc1ee33694bf5379b9f04865d2867a44ebf2735
taianaazevedo <taianazev@gmail.com>
2022-11-20 11:47:43 -0300

    soma/subtrai likes

--- src/components/Post.js
+++ src/components/Post.js
@@ -3,8 +3,8 @@ import { useState } from "react";
 
 export default function Post() {
     const post = [
-        { imgUser: "assets/img/meowed.svg", userName: "meowed", photo: "assets/img/gato-telefone.svg", likedBy: "assets/img/respondeai.svg", text1: "respondeai", text2: "101.523" },
-        { imgUser: "assets/img/barked.svg", userName: "barked", photo: "assets/img/dog.svg", likedBy: "assets/img/adorable_animals.svg", text1: "adorable_animals", text2: "99.159" }
+        { imgUser: "assets/img/meowed.svg", userName: "meowed", photo: "assets/img/gato-telefone.svg", likedBy: "assets/img/respondeai.svg", text1: "respondeai", text2: 101523 },
+        { imgUser: "assets/img/barked.svg", userName: "barked", photo: "assets/img/dog.svg", likedBy: "assets/img/adorable_animals.svg", text1: "adorable_animals", text2: 99159 }
     ]
 
     return (
@@ -19,30 +19,49 @@ function EachPost(props) {
 
     const [save, setSave] = useState("bookmark-outline");
     const [liked, setLiked] = useState("heart-outline");
-    const [colorLike, setColorLike] = useState("");
-/* função que marca ou desmarca como salvo */
-    function savePost(){
-        if(save === "bookmark-outline"){
+    const [color, setColor] = useState("");
+    const [countLikes, setCountLikes] = useState(props.text2);
+
+    /* função que marca ou desmarca como salvo */
+    function savePost() {
+        if (save === "bookmark-outline") {
             setSave("bookmark");
         } else {
             setSave("bookmark-outline");
         }
     }
-/* função que dá like ou tira o like do post*/
-    function likePost(){
-        if(liked === "heart-outline"){
+
+    
+    /* função que dá like ou tira o like do post e soma ou subtrai o like*/
+    function likePost() {
+        if (liked === "heart-outline") {
             setLiked("heart");
-            setColorLike("red");
+            setColor("red");
+            setCountLikes(countLikes + 1);
         } else {
             setLiked("heart-outline");
+            setColor("");
+            setCountLikes(countLikes - 1);
         }
+        
     }
+
+  
+    /* função que dá like clicando 2x na foto */
+    function doubleClick(event) {
+        if (event.detail === 2) {
+            setLiked("heart");
+            setColor("red");
+            setCountLikes(countLikes + 1);
+        }
+    };
+
 
     return (
         <div className="post" data-test="post">
             <div className="topo">
                 <div className="usuario">
-                    <img src={props.imgUser} data-test="post-image" />
+                    <img src={props.imgUser} />
                     {props.userName}
                 </div>
                 <div className="acoes">
@@ -51,13 +70,13 @@ function EachPost(props) {
             </div>
 
             <div className="conteudo">
-                <img src={props.photo} />
+                <img src={props.photo} onClick={doubleClick} data-test="post-image" />
             </div>
 
             <div className="fundo">
                 <div className="acoes">
                     <div>
-                        <ion-icon name={liked} style={{ color: {colorLike} }} onClick={likePost} data-test="like-post"></ion-icon>
+                        <ion-icon name={liked} style={{color: color }} onClick={likePost} data-test="like-post"></ion-icon>
                         <ion-icon name="chatbubble-outline"></ion-icon>
                         <ion-icon name="paper-plane-outline"></ion-icon>
                     </div>
@@ -69,7 +88,7 @@ function EachPost(props) {
                 <div className="curtidas">
                     <img src={props.likedBy} />
                     <div className="texto" data-test="likes-number">
-                        Curtido por <strong>{props.text1}</strong> e <strong> outras {props.text2} pessoas</strong>
+                        Curtido por <strong>{props.text1}</strong> e <strong> outras {countLikes} pessoas</strong>
                     </div>
                 </div>
             </div>
--- src/components/Usuario.js
+++ src/components/Usuario.js
@@ -5,34 +5,37 @@ export default function Usuario() {
     const [nome, setNome] = useState("Catana");
     const [img, setImg] = useState("assets/img/catanacomics.svg")
 
-    function changeName(){
+    /* função que altera o nome de usuário */
+    function changeName() {
         const resposta = prompt("Qual seu nome de usuário?");
-        if(resposta !== ""){
+        if (resposta !== "") {
             setNome(resposta);
         } else {
             prompt("Insira um nome válido");
-        }  
-        
-}
-    function changeImage(){
+        }
+
+    }
+    
+    /* função que muda a imagem do perfil */
+    function changeImage() {
         const resposta = prompt("Insira o link de sua imagem de perfil");
-        if(resposta !== ""){
+        if (resposta !== "") {
             setImg(resposta);
         } else {
             prompt("Insira um link válido");
-        } 
+        }
     }
 
-    return (        
-            <div className="usuario" data-test="user">            
-                <img src={img} data-test="profile-image" onClick={changeImage}/>
-                <div className="texto">
-                    <strong>{nome}</strong>
-                    <span data-test="name">
-                        {nome}
-                        <ion-icon name="pencil" data-test="edit-name" onClick={changeName}></ion-icon>
-                    </span>
-                </div>
+    return (
+        <div className="usuario" data-test="user">
+            <img src={img} data-test="profile-image" onClick={changeImage} />
+            <div className="texto">
+                <strong>{nome}</strong>
+                <span data-test="name">
+                    {nome}
+                    <ion-icon name="pencil" data-test="edit-name" onClick={changeName}></ion-icon>
+                </span>
             </div>
-        )
+        </div>
+    )
 }
